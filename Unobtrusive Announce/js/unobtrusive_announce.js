9b1959b953a509b828f89d284f777eb5999bd400
Bryan Garaventa <bryan.garaventa@whatsock.com>
2017-08-25 17:03:46 -0700

    Formatted code for better viewing

--- Unobtrusive Announce/js/unobtrusive_announce.js
+++ Unobtrusive Announce/js/unobtrusive_announce.js
@@ -6,19 +6,19 @@ Part of AccDC, a Cross-Browser JavaScript accessibility API, distributed under t
 
 (function(){
 
-// Offscreen text CSS rules
+	// Offscreen text CSS rules
 	var sraCSS =
 					{
-position: 'absolute',
-clip: 'rect(1px 1px 1px 1px)',
-clip: 'rect(1px, 1px, 1px, 1px)',
-clipPath: 'inset(50%)',
-padding: 0,
-border: 0,
-height: '1px',
-width: '1px',
-overflow: 'hidden',
-whiteSpace: 'nowrap'
+					position: 'absolute',
+					clip: 'rect(1px 1px 1px 1px)',
+					clip: 'rect(1px, 1px, 1px, 1px)',
+					clipPath: 'inset(50%)',
+					padding: 0,
+					border: 0,
+					height: '1px',
+					width: '1px',
+					overflow: 'hidden',
+					whiteSpace: 'nowrap'
 					};
 
 	window.announce = function(str, noRepeat, aggr){
@@ -33,61 +33,75 @@ whiteSpace: 'nowrap'
 						]);
 	};
 
-String.prototype.announce = function announce(strm, loop, noRep, aggr){
-if (String.announce.loaded){
-if (!String.announce.liveRendered && !aggr && String.announce.placeHolder){
-String.announce.liveRendered = true;
-document.body.appendChild(String.announce.placeHolder);
-}
-if (!String.announce.alertRendered && aggr && String.announce.placeHolder2){
-String.announce.alertRendered = true;
-document.body.appendChild(String.announce.placeHolder2);
-}
-}
-if (strm && strm.nodeName && strm.nodeType === 1) strm = getText(strm);
-var obj = strm || this,
-str = strm ? strm : this.toString();
-if (typeof str !== 'string') return obj;
-if (!loop && inArray(str, String.announce.alertMsgs) === -1) String.announce.alertMsgs.push(str);
-if ((String.announce.alertMsgs.length == 1 || loop)){
-var timeLength = String.announce.baseDelay + (String.announce.iterate(String.announce.alertMsgs[0], /\s|\,|\.|\:|\;|\!|\(|\)|\/|\?|\@|\#|\$|\%|\^|\&|\*|\\|\-|\_|\+|\=/g) * String.announce.charMultiplier);
-if (!(noRep && String.announce.lastMsg == String.announce.alertMsgs[0])){
-String.announce.lastMsg = String.announce.alertMsgs[0];
-if (aggr)
-String.announce.placeHolder2.innerHTML = String.announce.alertMsgs[0];
-else
-String.announce.placeHolder.innerHTML = String.announce.alertMsgs[0];
-}
-String.announce.alertTO = setTimeout(function(){
-String.announce.placeHolder.innerHTML = String.announce.placeHolder2.innerHTML = '';
-String.announce.alertMsgs.shift();
-if (String.announce.alertMsgs.length >= 1)
-String.prototype.announce(String.announce.alertMsgs[0], true, noRep, aggr);
-}, timeLength);
-}
-return obj;
+	String.prototype.announce = function announce(strm, loop, noRep, aggr){
+	if (String.announce.loaded){
+		if (!String.announce.liveRendered && !aggr && String.announce.placeHolder){
+			String.announce.liveRendered = true;
+			document.body.appendChild(String.announce.placeHolder);
+		}
+
+		if (!String.announce.alertRendered && aggr && String.announce.placeHolder2){
+			String.announce.alertRendered = true;
+			document.body.appendChild(String.announce.placeHolder2);
+		}
+	}
+
+	if (strm && strm.nodeName && strm.nodeType === 1)
+		strm = getText(strm);
+	var obj = strm || this, str = strm ? strm : this.toString();
+
+	if (typeof str !== 'string')
+		return obj;
+
+	if (!loop && inArray(str, String.announce.alertMsgs) === -1)
+		String.announce.alertMsgs.push(str);
+
+	if ((String.announce.alertMsgs.length == 1 || loop)){
+		var timeLength = String.announce.baseDelay + (String.announce.iterate(String.announce.alertMsgs[0],
+			/\s|\,|\.|\:|\;|\!|\(|\)|\/|\?|\@|\#|\$|\%|\^|\&|\*|\\|\-|\_|\+|\=/g) * String.announce.charMultiplier);
+
+		if (!(noRep && String.announce.lastMsg == String.announce.alertMsgs[0])){
+			String.announce.lastMsg = String.announce.alertMsgs[0];
+
+			if (aggr)
+				String.announce.placeHolder2.innerHTML = String.announce.alertMsgs[0];
+
+			else
+				String.announce.placeHolder.innerHTML = String.announce.alertMsgs[0];
+		}
+		String.announce.alertTO = setTimeout(function(){
+			String.announce.placeHolder.innerHTML = String.announce.placeHolder2.innerHTML = '';
+			String.announce.alertMsgs.shift();
+
+			if (String.announce.alertMsgs.length >= 1)
+				String.prototype.announce(String.announce.alertMsgs[0], true, noRep, aggr);
+		}, timeLength);
+	}
+	return obj;
 };
 
-String.announce = {
-alertMsgs: [],
-clear: function(){
-if (this.alertTO) clearTimeout(this.alertTO);
-this.alertMsgs = [];
-},
-baseDelay: 1000,
-charMultiplier: 10,
-lastMsg: '',
-iterate: function(str, regExp){
-var iCount = 0;
-str.replace(regExp, function(){
-iCount++;
-});
-return iCount;
-},
-loaded: false,
-liveRendered: false,
-alertRendered: false
-};
+	String.announce =
+					{
+					alertMsgs: [],
+					clear: function(){
+						if (this.alertTO)
+							clearTimeout(this.alertTO);
+						this.alertMsgs = [];
+					},
+					baseDelay: 1000,
+					charMultiplier: 10,
+					lastMsg: '',
+					iterate: function(str, regExp){
+						var iCount = 0;
+						str.replace(regExp, function(){
+							iCount++;
+						});
+						return iCount;
+					},
+					loaded: false,
+					liveRendered: false,
+					alertRendered: false
+					};
 
 	bind(window, 'load', function(){
 		if (!String.announce.placeHolder){
